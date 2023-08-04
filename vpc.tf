3ffba7ea4f9d1a5b4408dd3144171fef86f66656
babu517 <46353877+babu517@users.noreply.github.com>
2023-08-04 16:41:02 +0530

    vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -1,9 +1,9 @@
-resource "aws_vpc" "Jenkins-vpc-harish" {
-  cidr_block       = "10.0.0.0/16"
-  instance_tenancy = "default"
+#resource "aws_vpc" "Jenkins-vpc-harish" {
+#cidr_block       = "10.0.0.0/16"
+ # instance_tenancy = "default"
 
-  tags = {
-    Name = "demo-vpc"
-    Purpose = "Jenkins Demo"
-  }
-}
+  #tags = {
+   # Name = "demo-vpc"
+    #Purpose = "Jenkins Demo"
+ # }
+#}
