910377f2f140ac51dbbe8c4c95d983f1ef223466
babu517 <46353877+babu517@users.noreply.github.com>
2023-08-04 16:31:47 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "harish-waseem" {
   ami_id        = "ami-0df7a207adb9748c7"
-  instance_type = "x5.large"
+  instance_type = "r5.large"
 
   tags = {
     Name = "HelloWorld"
