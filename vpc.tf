85445b0f84ac890817e44b7e8ed8ef54647af9b4
babu517 <46353877+babu517@users.noreply.github.com>
2023-08-04 16:50:27 +0530

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -1,9 +1,18 @@
-#resource "aws_vpc" "Jenkins-vpc-harish" {
-#cidr_block       = "10.0.0.0/16"
- # instance_tenancy = "default"
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
 
-  #tags = {
-   # Name = "demo-vpc"
-    #Purpose = "Jenkins Demo"
- # }
-#}
+  name = "harish-vpc"
+  cidr = "10.0.0.0/16"
+
+  azs             = ["ap-southeast-1a", "ap-southeast-1b"]
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24"]
+
+  enable_nat_gateway = true
+  enable_vpn_gateway = true
+
+  tags = {
+    Terraform = "true"
+    Environment = "dev"
+  }
+}
